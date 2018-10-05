70dc2dacba64d47d797dabdb521b37791f27dd03
Andy Chuong <andy.chuong@colorado.edu>
2018-10-05 14:56:16 -0600

    added testing

--- src/routes/snacks.js
+++ src/routes/snacks.js
@@ -6,9 +6,11 @@ const control = require('../controllers/snacks')
 router.get('/', control.getAll)
 // http://localhost:3000/snacks/snacks/
 router.get('/:id', control.getOneSnack)
-//http  --json POST http://localhost:3000/snacks name=Chips+Ahoy
+// http  --json POST http://localhost:3000/snacks name=Chips+Ahoy
 router.post('/', control.createSnack)
+// http --json PUT http://localhost:3000/snacks/$id name=Wings
 router.put('/:id', control.updateSnack)
+// http DELETE http://localhost:3000/snacks/$id
 router.delete('/:id', control.deleteSnack)
 
 module.exports = router
